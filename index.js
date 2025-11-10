5a65de2a2905ce8cbf6cf328f3308f2f39f01181
WillianBoita <wkboita@gmail.com>
2025-11-10 11:51:23 -0300

    Classificador de nível

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-pai tron
+const vitorias = parseInt(process.argv[2]);
+const derrotas = parseInt(process.argv[3]);
+let nivel;
+
+const saldoVitorias = vitorias - derrotas
+
+if (vitorias <= 10) {
+    nivel = "Ferro"
+} else if (vitorias <= 20) {
+    nivel = "Bronze"
+} else if (vitorias <= 50) {
+    nivel = "Prata"
+} else if (vitorias <= 80) {
+    nivel = "Ouro"
+} else if (vitorias <= 90) {
+    nivel = "Diamante"
+} else if (vitorias <= 100) {
+    nivel = "Landário"
+} else {
+    nivel = "Imortal"
+}
+
+console.log("O Herói tem saldo de " + saldoVitorias + " está no nível de " + nivel)
